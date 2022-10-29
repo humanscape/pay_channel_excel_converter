c3add32a6a0233ecb8afebedeaf151d39e5bd5a4
humanscape-chan <chan@humanscape.io>
2022-10-29 19:18:53 +0900

    feat: add error handler for word_conveter for make sure commnet add

--- comment_converter.py
+++ comment_converter.py
@@ -7,23 +7,25 @@ regex_n_people = re.compile("[0-9]인")
 def word_converter(words, nick_to_human):
     USAGE = ("점심", "저녁", "식사")
     result = {"usage": None, "kor_names": ""} #, "cell_counter": defaultdict(int)}
-    for word in words:
-        if (not word or word == " ") or (regex_n_people.match(word)):
-            continue
+    try: 
+        for word in words:
+            if (not word or word == " ") or (regex_n_people.match(word)):
+                continue
 
-        if any(usage in word for usage in USAGE) and result["usage"] is None:
-            result["usage"] = word
-            continue
+            if any(usage in word for usage in USAGE) and result["usage"] is None:
+                result["usage"] = word
+                continue
 
-        if word not in nick_to_human:
-            return {"usage": None, "kor_names": None} #, "cell_counter": None}
+            if word not in nick_to_human:
+                return {"usage": None, "kor_names": None} #, "cell_counter": None}
 
-        if nick_to_human[word]:
-            if result["kor_names"]:
-                result["kor_names"] += ","
-            result["kor_names"] += nick_to_human[word].kor_name
-            # result["cell_counter"][nick_to_human[word].cell_name] += 1
-
+            if nick_to_human[word]:
+                if result["kor_names"]:
+                    result["kor_names"] += ","
+                result["kor_names"] += nick_to_human[word].kor_name
+                # result["cell_counter"][nick_to_human[word].cell_name] += 1
+    except:
+        return {"usage": None, "kor_names": ""}
     return result
 
 
